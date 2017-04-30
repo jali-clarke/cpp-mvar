803b4885ed75f317abc63cd1ec93f16dec37097e
jali-clarke <jinnah.ali-clarke@outlook.com>
2017-04-30 17:06:44 -0400

    initial implementation

--- MVar.h
+++ MVar.h
@@ -3,29 +3,42 @@
 
 #include <mutex>
 
-// unlocked = open (no data)
-// locked = closed (has data)
+// an MVar is initially created as open
 
 template <typename T>
 class MVar{
 private:
-    std::mutex openLock; // initially unlocked
+    // neither of these locks will be both open or closed at the same time
+    std::mutex openLock; // should be initially locked
+    std::mutex closeLock; // should be initially unlocked
     T data;
 
 public:
-    MVar(){}
+    MVar(){
+        openLock.lock();
+    }
 
     const T& get() const{
-
+        openLock.lock() // box is now open
+        closeLock.unlock() // ditto
+        return data;
     }
 
     void put(const T& newData){
-
+        closeLock.lock() // box is closed
+        data = newData;
+        openLock.unlock() // ditto
     }
 
     template <typename Func>
     void modify(Func f){
+        openLock.lock() // box is now open
+        closeLock.unlock() // ditto
 
+        data = f(data);
+
+        closeLock.lock(); // box is closed
+        openLock.unlock(); // ditto
     }
 };
 
